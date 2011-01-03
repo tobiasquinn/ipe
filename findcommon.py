1af7cd959dbe0fca84dd71f9a140ffdff075a087
Tobias Quinn <tobias@tobiasquinn.com>
2011-01-03 02:44:04 +0000

    added tests

--- findcommon.py
+++ findcommon.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 class FindCommonStart:
     # feed a number of strings in and allow the return of the common start string
     __common = None
@@ -9,7 +11,26 @@ class FindCommonStart:
             return
         else:
             print("New common start current count is %d" % (len(self.__common)))
-            # search for the common start string
+            # find the furthest point our input string 
 
     def getCommon(self):
         return self.__common
+
+if __name__ == '__main__':
+    def runTest(strings):
+        fcs = FindCommonStart()
+        c = 0
+        for s in strings:
+            fcs.processString(s)
+            print ("T%d:%s" % (c, fcs.getCommon()))
+            c = c + 1
+        print("Result:%s" % (fcs.getCommon()))
+
+    print("FindCommonStart Tests")
+    testStrings = ["This is a test blah blah deblah",
+                  "This is a ",
+                  "This",
+                  "This is a test blah blah"]
+    runTest(testStrings)
+    testStrings.reverse()
+    runTest(testStrings)
